af3caa02fe63b054a84b152e00191a9ab56c63da
cc <44751363+zzfn@users.noreply.github.com>
2023-09-09 23:12:33 +0800

    feat: oss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  assetPrefix:
+      process.env.NODE_ENV === 'production' && !process.env.VERCEL
+          ? 'https://cdn.zzfzzf.com/monitor'
+          : '/',
 };
 
 module.exports = nextConfig;
